7173b9b93560e666e9b7a7ed518a2f12eafad3b8
Tolstunov <iscander161@yandex.ru>
2025-08-17 21:24:35 +0300

    Тест сборки приложения

---
+++ Application/Core/Src/Main.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file Main.cpp
+ * @author Tolstunov Alexander (iscander161@yandex.ru)
+ * @brief
+ * @date 17 August 2022
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include <iostream>
+
+/**
+ * Главная функция приложения
+ * @param argc - Количество аргументов приложения
+ * @param argvs - Аргументы приложения
+ * @return int - Результат выполнения приложения
+ */
+int main(int argc, char** argvs) {
+    std::cout << "Hello World!\n" << std::endl;
+    return EXIT_SUCCESS;
+}
